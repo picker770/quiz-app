0f66729b7de3cca87af12cebca4d162ca91cb936
picker770 <arehman897@gmail.com>
2026-01-25 18:26:08 +0000

    Add quiz functionality in question.js file alongside run my test to prove that quizData has a length of 4 using jsdom/jest

--- scripts/question.js
+++ scripts/question.js
@@ -37,4 +37,81 @@ const quizData = [
   },
 ];
 
+// Quiz state
+
+let currentQuiz = 0;
+let score = 0;
+
+
+// Functions for quiz
+
+function loadQuiz () {
+  const questionEl = document.getElementById('question');
+  const a_text = document.getElementById('a_text');
+  const b_text = document.getElementById('b_text');
+  const c_text = document.getElementById('c_text');
+  const d_text = document.getElementById('d_text');
+
+  if (!questionEl || !a_text || !b_text || !c_text || !d_text) return;
+
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  const answerEls = document.querySelectorAll('.answer');
+  answerEls.forEach(answerEl => answerEl.checked = false);
+}
+
+function getSelected() {
+  const answerEls = document.querySelectorAll('.answer');
+  let answer;
+  answerEls.forEach(answerEl => {
+      if (answerEl.checked) answer = answerEl.id;
+  });
+
+    return answer;
+}
+
+// Browser Initialization
+
+if (typeof document !== 'undefined') {
+   const quiz = document.getElementById('quiz');
+   const submitBtn = document.getElementById('submit');
+
+   if (submitBtn) {
+      loadQuiz();
+
+       submitBtn.addEventListener('click', () => {
+          const answer = getSelected();
+
+          if(answer) {
+            if (answer === quizData[currentQuiz].correct) score++;
+
+            currentQuiz++;
+
+            if(currentQuiz < quizData.length) {
+                loadQuiz();
+            } else {
+                if(quiz) {
+                    quiz.innerHTML = `
+                    <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                    <button onclick="location.reload()">Reload</button>
+                    `;
+                }
+            }
+          }
+       });
+   }
+
+}
+
+// Exports for testing
+
 module.exports = { quizData };
